ea3e9e329f747e60c3b75c5804313b8266be6e97
Justelex <github.acc@maik-nauheim.de>
2015-11-07 21:32:55 +0100

    Added Twitter streaming api

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+require('../../cred.js');
 
 app.get('/', function(req, res){
    dir = req.params.dir,  
@@ -17,6 +18,28 @@ io.on('connection', function(socket){
 	io.emit('message', { for: 'everyone' });
 	
  });
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+   consumer_key: ckey,
+   consumer_secret: csecret,
+   access_token_key: akey,
+   access_token_secret: asecret
+});
+
+client.stream('statuses/filter', {track: 'bindercon'}, function(stream) {
+   stream.on('data', function(tweet) {
+      console.log(tweet.text);
+   });
+	 
+   stream.on('error', function(error) {
+      throw error;
+   });
+});
+
+
 http.listen(3000, function(){
     console.log('listening on *:3000');
   });
+
