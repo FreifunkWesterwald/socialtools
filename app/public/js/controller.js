b0067e8dc856f47fca1ec4cb85e2ad8f31b236a0
Moritz <moritz.kneilmann@gmx.de>
2015-11-08 01:18:18 +0100

    Js function for twitter 'parsing'

--- app/public/js/controller.js
+++ app/public/js/controller.js
@@ -1,17 +1,24 @@
 
 twitterwall.controller('tweetCtrl', function($scope,socket){
 	$scope.tweets = [];
-	
+
 	socket.on("newTweet",  function(msg){
 
-	 
-	  $scope.tweets.unshift(msg);
+	  $scope.tweets.unshift(stringToTwitter(msg));
 
-	  $scope.tweets = $scope.tweets.slice(0,5);		
+	  $scope.tweets = $scope.tweets.slice(0,5);
 
-	  console.log("Länge der Tweetwall:", $scope.tweets.length);	  
+	  console.log("Länge der Tweetwall:", $scope.tweets.length);
 	  console.log("after push: ",$scope.tweets);
+
+	  function stringToTwitter (string){
+		    string.replace(/(#|@)\w*/gi,
+		    function (t){
+		        var href = "https://twitter.com/" + t;
+		        return "<a href='" + href + "'>" + t + "</a>";
+		    })
+		};
 	});
-	
+
 
 });
