64ad00ac946737545092aa674fb68bf2b6578c28
Moritz <moritz.kneilmann@gmx.de>
2015-11-08 12:02:22 +0100

    New Hastags and 6 tweets per page

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var tweetcache = [];
-var tweettrack = 'heartstone,orr,openrheinruhr,freifunk,nma2015';
+var tweettrack = '#Freifunk, #ORR, #ORR2015, #OpenRheinRuhr';
 require('../../cred.js')
 app.use('/', express.static('../app/public'));
 app.use('/public/css', express.static('../app/public/css'));
@@ -12,13 +12,13 @@ app.use('/public/js', express.static('../app/public/js'));
 
 
 app.get('/', function(req, res){
-   dir = req.params.dir,  
+   dir = req.params.dir,
    res.sendFile('app/index.html', {'root': '../'});
 });
 
 io.on('connection', function(socket){
     console.log('a user connected');
-    for (i = 0; i < tweetcache.length; i++) { 
+    for (i = 0; i < tweetcache.length; i++) {
     	io.emit('newTweet', (tweetcache[i]));
 
     }
@@ -34,14 +34,14 @@ var client = new Twitter({
    access_token_secret: asecret
 });
 
-client.stream('statuses/filter', {track: 'Hearthstone'}, function(stream) {
+client.stream('statuses/filter', {track: '@Freifunk, #Freifunk, @FreifunkWW, #FreifunkWW, #ORR, #ORR15, #ORR2015, #OpenRheinRuhr'}, function(stream) {
    stream.on('data', function(tweet) {
       tweetcache.unshift(tweet);
-      tweetcache = tweetcache.slice(0,5);
+      tweetcache = tweetcache.slice(0,6);
       console.log(tweet);
       io.emit('newTweet', (tweet));
    });
-	 
+
    stream.on('error', function(error) {
       throw error;
    });
@@ -51,4 +51,3 @@ client.stream('statuses/filter', {track: 'Hearthstone'}, function(stream) {
 http.listen(3000, function(){
     console.log('listening on *:3000');
   });
-
